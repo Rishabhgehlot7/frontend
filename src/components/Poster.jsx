963c49af042113ff0ac73b7b28b1e1834a5e121b
rishabh gehlot <rishabh3x@gmail.com>
2024-02-25 18:22:37 +0530

    postercommit

--- src/components/Poster.jsx
+++ src/components/Poster.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Poster = () => {
     return (
-        <div className=' w-screen h-auto p-5 max-w-[1270px] flex justify-between'>
+        <div className=' w-screen h-auto p-5 flex justify-between'>
             <div className=' bg-red-700 text-white p-5 text-2xl text-center'>PVC Cards – Order PVC Card at Just 50 Rs for Your Paper Card</div>
         </div>
     )
