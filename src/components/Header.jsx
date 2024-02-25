fbbf26e921cd46ffe0d21ce59948ba3292c43730
rishabh gehlot <rishabh3x@gmail.com>
2024-02-25 20:24:08 +0530

    commit card4

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,16 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
+import CardItem from './CardItem'
 
 export const Header = () => {
+    const [showCard, setShowCard] = useState(false)
+
+    const showCardMethod = () => {
+        setShowCard(!showCard)
+    }
+    useEffect(() => {
+
+    }, [showCard])
     return (
         <div className=' w-screen h-auto flex flex-col md:block hidden'>
             <div className='flex bg-red-800 w-full h-full p-1 justify-between text-white'>
@@ -20,7 +29,7 @@ export const Header = () => {
                     <button className=' bg-orange-600 p-1 text-white'>search</button>
                 </div>
                 <button className='p-1'>LOGIN/REGISTER</button>
-                <button className='bg-red-600 py-1 px-3 rounded-lg text-white border-none'>Card</button>
+                <button className='bg-red-600 py-1 px-3 rounded-lg text-white border-none' onClick={showCardMethod}>Card</button>
             </div>
             <ul className='list-none flex flex-wrap  bg-gray-500 w-full h-full'>
                 <li className=' font-medium p-2 text-xs'>PVC AADHAR CARD</li>
@@ -31,6 +40,18 @@ export const Header = () => {
                 <li className=' font-medium p-2 text-xs'>PVC AYUSHMAN BHARAT CARD</li>
                 <li className=' font-medium p-2 text-xs'>PVC ESHRAM CARD</li>
                 <li className=' font-medium p-2 text-xs'>SHOP</li>
+            </ul>
+            <ul className={`list-none h-full flex flex-col items-center bg-gray-100 shadow-2xl w-[50%] fixed top-0 ${showCard ? "right-0 transition duration-150" : " right-[50%] hidden"}`}>
+                <button onClick={showCardMethod} className=' absolute right-0 p-3 font-bold'>X</button>
+                <div className=' h-8'></div>
+                <h2 className=' text-2xl text-center py-3'>CARD</h2>
+                <div className=' w-10 h-1 bg-gray-600'></div>
+                <CardItem />
+                <hr />
+                <div className='py-5'>Subtotal: ₹250.00</div>
+                <button className=' w-[80%] p-2 bg-red-700 text-white my-2'>VIEW CARD</button>
+                <button className=' w-[80%] p-2 bg-orange-500 text-white my-2'>CHECKOUT</button>
+                <hr />
             </ul>
         </div>
     )
